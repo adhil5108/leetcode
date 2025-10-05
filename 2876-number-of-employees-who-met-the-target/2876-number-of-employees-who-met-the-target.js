01e3d4dfc2267b1ce296a0836ca2dc642a7e8a26
adhil <adhil6779@gmail.com>
2025-10-05 09:26:13 +0530

    Time: 0 ms (100%), Space: 56.3 MB (28.79%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let count =0
+    for(let x of hours){
+        if(x>=target){
+            count=count+1
+        }
+    }
+    return count
+};
