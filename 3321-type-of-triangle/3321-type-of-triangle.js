02137c617457aed7e6b04577a698e9374a207acc
adhil <adhil6779@gmail.com>
2025-10-06 08:46:06 +0530

    Time: 0 ms (100%), Space: 55.9 MB (15.66%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function (nums) {
+    if (nums.length != 3) return "none";
+
+    const [a, b, c] = nums;
+    if (a + b <= c || a + c <= b || b + c <= a) return "none";
+
+    if (a === b && b === c) return "equilateral";
+    else if (a === b || a === c || b === c) return "isosceles";
+    else return "scalene";
+};
