36d997a11143861e231c85447073b407bfe5cb6a
adhil <adhil6779@gmail.com>
2025-10-17 17:31:02 +0530

    Time: 5 ms (22.26%), Space: 66.4 MB (7.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,17 +3,16 @@
  * @return {number}
  */
 function maxProfit(prices) {
-  let minPrice = 2000000000;
-  let maxProfit = 0;
+  let minPrice = 2000000000
+  let maxProfit = 0
 
   for (let price of prices) {
     if (price < minPrice) {
-      minPrice = price;
+      minPrice = price
     } else {
         const profit = price - minPrice;
-      maxProfit = Math.max(maxProfit, profit);
+      maxProfit = Math.max(maxProfit, profit)
     }
   }
-
   return maxProfit;
 }
