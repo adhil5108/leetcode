9ba2b03ceadd896e0fcac9b9812f0f987c380808
adhil <adhil6779@gmail.com>
2025-10-24 14:58:38 +0530

    Time: 11 ms (26.61%), Space: 65.4 MB (28%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,8 +2,8 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    let y=String(x)
-    let z=y.split("").reverse().join("")
-  return y==z
+var isPalindrome = function (x) {
+    let y = String(x)
+    let z = y.split("").reverse().join("")
+    return y == z
 };
