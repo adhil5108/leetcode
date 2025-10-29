f6306860264a40bebba822ad2b8dcda1ca40c469
adhil <adhil6779@gmail.com>
2025-10-29 09:18:27 +0530

    Time: 1 ms (17.39%), Space: 53.8 MB (76.09%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+var xorOperation = function (n, start) {
+    let arr = []
+    for (let i = 0; i < n; i++) {
+        arr[i] = start + 2 * i
+    }
+    return arr.reduce((a,b)=>{return a^b},0)
+};
