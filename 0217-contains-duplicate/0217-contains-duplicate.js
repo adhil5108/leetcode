49d2daf0334d224df0f420a09eaa15f41556b254
adhil <adhil6779@gmail.com>
2025-11-05 09:25:57 +0530

    Time: 75 ms (9.37%), Space: 65.9 MB (97.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,15 +2,22 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    const seen = new Set();
+var containsDuplicate = function (nums) {
+    // const seen = new Set();
 
-    for (let num of nums) {
-        if (seen.has(num)) {
-            return true; 
+    // for (let num of nums) {
+    //     if (seen.has(num)) {
+    //         return true; 
+    //     }
+    //     seen.add(num);
+    // }
+
+    // return false;
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == nums[i + 1]) {
+            return true
         }
-        seen.add(num);
     }
-
-    return false;
+    return false
 };
