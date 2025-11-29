fd9a9b90b4b549cd3c711f734b9b2fd963147929
adhil <adhil6779@gmail.com>
2025-11-29 10:38:17 +0530

    Time: 8 ms (10.34%), Space: 45.6 MB (38.24%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        String s = Integer.toString(x);
+        String rev = new StringBuilder(s).reverse().toString();
+
+        return s.equals(rev);
+    }
+}
