f07dc889f9834ddeb368d3a4b953deca9b0362c8
adhil <adhil6779@gmail.com>
2025-12-31 15:28:21 +0530

    Time: 4 ms (13.61%), Space: 59.4 MB (9.42%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfMultiples = function (n) {
+    let arr = [];
+   
+    for (let i = 0; i <= n; i++) {
+         if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+            arr.push(i);
+        }
+    }
+   
+    let sum =arr.reduce((s,i)=>s+i);
+    return sum;
+};
