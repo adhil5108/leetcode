dca362340e01e3640f451987a76cfdf5b69fb272
adhil <adhil6779@gmail.com>
2026-01-12 10:47:17 +0530

    Time: 1 ms (30.91%), Space: 53.5 MB (98.18%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.js
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.js
@@ -7,15 +7,14 @@
 var divideString = function (s, k, fill) {
     let ret = [];
     for (let i = 0; i < s.length; i += k) {
-        ret.push(s.slice(i, i + k));
-        if (i <= i + k) { continue; }
+        ret.push(s.slice(i, i + k));   
     }
     if (ret[ret.length - 1].length != k) {
-        let lastone = ret.pop().split("");
+        let lastone = ret.pop();
         for (let i = lastone.length; i < k; i++) {
-            lastone.push(fill);
+            lastone=lastone+fill;
         }
-        ret.push(lastone.join(""));
+        ret.push(lastone);
     }
     return ret;
 };
