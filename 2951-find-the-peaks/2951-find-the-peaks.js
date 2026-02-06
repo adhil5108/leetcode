528a1a52494394dc791de7d892129ec254210ba2
adhil <adhil6779@gmail.com>
2026-02-06 16:53:25 +0530

    Time: 0 ms (100%), Space: 58.8 MB (7.84%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.js
+++ 2951-find-the-peaks/2951-find-the-peaks.js
@@ -4,9 +4,8 @@
  */
 var findPeaks = function (mountain) {
     let ret = [];
-    for (let i = 0; i < mountain.length; i++) {
-        if (i == 0 || i == mountain.length - 1) { continue; }
-        else if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
+    for (let i = 1; i < mountain.length-1; i++) {
+        if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
             ret.push(i);
         }
     }
